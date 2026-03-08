e2e42cfd6426b1034338ebb78040971f2a342ac3
Vanpersie-debug <kayitarevanny@gmail.com>
2026-03-08 18:02:38 +0200

    update

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../../context/Authcontext';
 
 function Login({ show, handleClose }) {
   if (!show) return null;
